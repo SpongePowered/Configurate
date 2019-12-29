93f4458d5e43f04b71556704b7ad6a21f23b14d3
Zach Levis <zml@aoeu.xyz>
2019-12-28 18:21:59 -0800

    ext-kotlin: Provide basic kotlin extension methods
    This aims to be a relatively conservative attempt to provide extensions
    that most projects using Configurate in kotlin would want to add at some
    point.

---
+++ configurate-ext-kotlin/src/main/kotlin/org/spongepowered/configurate/kotlin/extensions.kt
@@ -0,0 +1,59 @@
+/*
+ * Configurate
+ * Copyright (C) zml and Configurate contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.spongepowered.configurate.kotlin
+
+import com.google.common.reflect.TypeToken
+import org.spongepowered.configurate.ConfigurationNode
+import org.spongepowered.configurate.objectmapping.ObjectMappingException
+
+operator fun <T: ConfigurationNode<T>> T.get(path: Any): T {
+    return getNode(path)
+}
+
+operator fun <T: ConfigurationNode<T>> T.get(vararg path: Any): T {
+    return getNode(*path)
+}
+
+operator fun <T: ConfigurationNode<T>> T.set(vararg path: Any, value: Any?) {
+    getNode(*path).value = value
+}
+
+/**
+ * Multi level contains
+ */
+operator fun ConfigurationNode<*>.contains(path: Array<Any>): Boolean {
+    return !getNode(*path).isVirtual()
+}
+
+/**
+ * Contains for a single level
+ *
+ * @param path a single path element
+ */
+operator fun ConfigurationNode<*>.contains(path: Any): Boolean {
+    return !getNode(path).isVirtual()
+}
+
+@Throws(ObjectMappingException::class)
+inline fun <T: ConfigurationNode<T>, reified V> T.get(default: V? = null): V? {
+    return getValue(object : TypeToken<V>() {}, default)
+}
+
+@Throws(ObjectMappingException::class)
+inline fun <T: ConfigurationNode<T>, reified V> T.set(value: V?) {
+    setValue(object : TypeToken<V>() {}, value)
+}
