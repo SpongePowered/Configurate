bff20a64bd2950e4c8eeefa383afdfe9172808ff
Zach Levis <zml@aoeu.xyz>
2020-04-05 23:07:45 -0700

    Add a CLI tool to inspect Configurate's nodes
    This gives a way to quickly visualize how Configurate sees a
    configuration file.
    Tool can be expanded in the future -- it may be useful for format
    conversions, reformatting files, etc

---
+++ configurate-tool/build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    application
+    kotlin("jvm") version "1.3.71"
+}
+
+
+dependencies {
+    // Configurate
+    implementation(project(":configurate-xml"))
+    implementation(project(":configurate-yaml"))
+    implementation(project(":configurate-gson"))
+    implementation(project(":configurate-hocon"))
+    implementation(project(":configurate-ext-kotlin"))
+
+    // Libraries
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("com.github.ajalt:clikt:2.6.0")
+    implementation("com.github.ajalt:mordant:1.2.1")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.useIR = true
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+                mapOf("Specification-Version" to project.version,
+                "Implementation-Version" to project.version)
+        )
+    }
+}
+
+application {
+    mainClassName = "org.spongepowered.configurate.tool.ToolKt"
+}
---
+++ configurate-tool/src/main/kotlin/org/spongepowered/configurate/tool/Tool.kt
@@ -0,0 +1,205 @@
+/*
+ * Configurate
+ * Copyright (C) zml and Configurate contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.spongepowered.configurate.tool
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.CliktError
+import com.github.ajalt.clikt.core.context
+import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.versionOption
+import com.github.ajalt.clikt.parameters.types.enum
+import com.github.ajalt.clikt.parameters.types.int
+import com.github.ajalt.clikt.parameters.types.path
+import com.github.ajalt.mordant.TermColors
+import ninja.leaping.configurate.ConfigurationNode
+import ninja.leaping.configurate.ValueType
+import ninja.leaping.configurate.attributed.AttributedConfigurationNode
+import ninja.leaping.configurate.commented.CommentedConfigurationNode
+import ninja.leaping.configurate.gson.GsonConfigurationLoader
+import ninja.leaping.configurate.hocon.HoconConfigurationLoader
+import ninja.leaping.configurate.loader.ConfigurationLoader
+import ninja.leaping.configurate.loader.HeaderMode
+import ninja.leaping.configurate.xml.XMLConfigurationLoader
+import ninja.leaping.configurate.yaml.YAMLConfigurationLoader
+import org.yaml.snakeyaml.DumperOptions
+import java.io.IOException
+import java.nio.charset.Charset
+import java.nio.charset.StandardCharsets
+import java.nio.file.FileSystems
+
+
+private val t = TermColors()
+
+val HAS_UTF8 = Charset.defaultCharset() == StandardCharsets.UTF_8
+
+const val INDENT = "  "
+
+val CHILD_NODE = t.brightCyan(if (HAS_UTF8) "┣" else "+")
+val CHILD_CONT = t.brightCyan(if (HAS_UTF8) "┃" else "|")
+val CHILD_END = t.brightCyan(if (HAS_UTF8) "┗" else "L")
+val SPLIT = t.bold(if (HAS_UTF8) "╺" else "-")
+
+fun heading(text: String) = t.bold(t.brightBlue(text))
+
+class Tool : CliktCommand(help = """
+    This tool displays the Configurate data structures read from a config file
+    
+    This helps to understand how Configurate understands its data
+""".trimIndent()) {
+    init {
+        versionOption(this::class.java.`package`.implementationVersion)
+        context {
+            autoEnvvarPrefix = "CONFIGURATE"
+        }
+        subcommands(Xml(), Yaml(), Json(), Hocon())
+    }
+    override fun run() = Unit
+}
+
+sealed class FormatSubcommand<N: ConfigurationNode>: CliktCommand() {
+    val path by argument(help="Location of the file to read").path(mustExist = true, canBeDir = false)
+    val header by option("--header-mode", help="How to read a header from this file").enum<HeaderMode>().default(HeaderMode.PRESERVE)
+
+    /**
+     * Create a new loader instance based on provided arguments
+     */
+    abstract fun createLoader(): ConfigurationLoader<N>
+
+    override fun run() {
+        echo("Current dir: ${FileSystems.getDefault().getPath(".").toAbsolutePath()}")
+        val loader = createLoader()
+        try {
+            val node = loader.load()
+            dumpTree(node, "")
+        } catch (e: IOException) {
+            throw CliktError("Unable to load configuration", e)
+        }
+    }
+
+    private fun dumpTree(node: ConfigurationNode, prefix: String = "") {
+        fun write(vararg content: Any?) {
+            echo(prefix + content.joinToString(" "))
+        }
+
+        fun enterChild(iter: Iterator<*>, child: ConfigurationNode): String {
+            val childPrefix: String
+            val entryPrefix: String
+
+            if (iter.hasNext()) {
+                childPrefix = "$CHILD_CONT  "
+                entryPrefix = CHILD_NODE
+            } else {
+                childPrefix = INDENT
+                entryPrefix = CHILD_END
+            }
+
+            write(entryPrefix, heading(child.key?.toString() ?: "(unnamed)"), SPLIT, child.valueType)
+            return prefix + childPrefix
+        }
+
+        if (node is AttributedConfigurationNode) {
+            write(heading("Tag name"), SPLIT, node.tagName)
+            val attributes = node.attributes
+            if (!attributes.isEmpty()) {
+                write(heading("Attributes"), SPLIT, attributes.map { (k, v) -> t.gray("\"${t.white(k)}\"=\"${t.white(v)}\"") }.joinToString(", "))
+            }
+        }
+        if (node is CommentedConfigurationNode) {
+            node.comment.ifPresent {
+                write(heading("Comment"), SPLIT, it)
+            }
+        }
+        when (node.valueType) {
+            ValueType.LIST -> node.childrenList.iterator().also {
+                while (it.hasNext()) {
+                    val child = it.next()
+                    val nextPrefix = enterChild(it, child)
+                    dumpTree(child, nextPrefix)
+                }
+            }
+            ValueType.MAP -> node.childrenMap.iterator().also {
+                while (it.hasNext()) {
+                    val (_, value) = it.next()
+                    val nextPrefix = enterChild(it, value)
+
+                    dumpTree(value, nextPrefix)
+                }
+            }
+            ValueType.SCALAR -> write(heading("Value"), SPLIT, node.value.toString().replace(Regex("(\r?\n)"), "$1$prefix    "))
+            ValueType.NULL -> write(heading("Value: "), SPLIT, t.gray("(null)"))
+        }
+    }
+}
+
+class Xml : FormatSubcommand<AttributedConfigurationNode>() {
+    private val indent by option("-i", "--indent", help = "How much to indent when outputting").int().default(2)
+    private val includeXmlDeclaration by option("-x", "--xml-declaration", help = "Whether to include the XML declaration").flag("-X", default = true)
+    private val writeExplicitType by option("-t", "--explicit-type", help = "Include explicit type information").flag("-T", default = false)
+
+    // TODO: Schemas
+
+    override fun createLoader(): ConfigurationLoader<AttributedConfigurationNode> {
+        return XMLConfigurationLoader.builder()
+                .setPath(this.path)
+                .setHeaderMode(header)
+                .setIndent(indent)
+                .setIncludeXmlDeclaration(includeXmlDeclaration)
+                .setWriteExplicitType(writeExplicitType)
+                .build()
+    }
+}
+
+class Yaml: FormatSubcommand<ConfigurationNode>() {
+    private val indent by option("-i", "--indent", help = "How much to indent when outputting").int().default(4)
+    private val flowStyle by option("-f", "--flow", help = "What flow style to use").enum<DumperOptions.FlowStyle>().default(DumperOptions.FlowStyle.AUTO)
+    override fun createLoader(): ConfigurationLoader<ConfigurationNode> {
+        return YAMLConfigurationLoader.builder()
+                .setPath(path)
+                .setIndent(indent)
+                .setFlowStyle(flowStyle)
+                .build()
+    }
+}
+
+class Json: FormatSubcommand<ConfigurationNode>() {
+    private val lenient by option("-l", "--lenient", help = "Parse JSON leniently").flag("-L", "--strict", default = true)
+    private val indent by option("-i", "--indent", help = "How much to indent when outputting").int().default(4)
+    override fun createLoader(): ConfigurationLoader<ConfigurationNode> {
+        return GsonConfigurationLoader.builder()
+                .setPath(path)
+                .setLenient(lenient)
+                .setIndent(indent)
+                .build()
+    }
+}
+
+class Hocon: FormatSubcommand<CommentedConfigurationNode>() {
+
+    override fun createLoader(): ConfigurationLoader<CommentedConfigurationNode> {
+        return HoconConfigurationLoader.builder()
+                .setHeaderMode(header)
+                .setPath(path)
+                .build()
+    }
+}
+
+fun main(args: Array<String>) = Tool()
+        .main(args)
