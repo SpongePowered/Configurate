cba14be0b2c5d801dd73bfe52fbbf25fd7c13e03
zml <zml@stellardrift.ca>
2020-04-15 20:30:50 -0700

    Rename platform module to bom

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ private val prefix = "configurate"
 
 rootProject.name = "$prefix-parent"
 
-listOf("core", "tool", "ext-kotlin", "platform",
+listOf("core", "tool", "ext-kotlin", "bom",
         "gson", "hocon", "jackson", "xml", "yaml").forEach {
     include("$prefix-$it")
 }
