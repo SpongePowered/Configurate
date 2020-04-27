d54c24039b518b1fe0056271e3c9c914c9ad416f
Zach Levis <zml@aoeu.xyz>
2020-04-26 21:27:01 -0700

    Set up publishing javadocs to GH pages

--- buildSrc/src/main/kotlin/org/spongepowered/configurate/build/Extensions.kt
+++ buildSrc/src/main/kotlin/org/spongepowered/configurate/build/Extensions.kt
@@ -2,6 +2,19 @@ package org.spongepowered.configurate.build
 
 import org.gradle.api.artifacts.Dependency
 import org.gradle.api.artifacts.dsl.DependencyHandler
+import org.gradle.api.tasks.javadoc.Javadoc
+import org.gradle.external.javadoc.StandardJavadocDocletOptions
+
+enum class Versions(val version: String) {
+    GUAVA("25.1-jre"),
+    HOCON("1.4.0"),
+    SNAKEYAML("1.26"),
+    JACKSON("2.10.3");
+
+    override fun toString(): String {
+        return version
+    }
+}
 
 /**
  * Create a dependency on a specific component in this project
@@ -12,4 +25,21 @@ fun DependencyHandler.configurate(component: String): Dependency {
 
 fun DependencyHandler.core(): Dependency {
     return configurate("core")
+}
+
+fun Javadoc.applyCommonAttributes() {
+    val options = this.options
+    options.encoding = "UTF-8"
+    if (options is StandardJavadocDocletOptions) {
+        options.links(
+                "https://guava.dev/releases/${Versions.GUAVA}/api/docs/",
+                "https://lightbend.github.io/config/latest/api/",
+                "https://fasterxml.github.io/jackson-core/javadoc/2.10/",
+                "https://checkerframework.org/api/",
+                "https://docs.oracle.com/javase/8/docs/api"
+        )
+        options.source = targetVersion.toString()
+        options.addBooleanOption("html5", true)
+        options.linkSource()
+    }
 }
--- configurate-core/build.gradle.kts
+++ configurate-core/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.spongepowered.configurate.build.Versions
+
 plugins {
   id("org.spongepowered.configurate-component")
 }
@@ -10,8 +12,8 @@ java {
 }
 
 dependencies {
-  api("com.google.guava:guava:25.1-jre")
+  api("com.google.guava:guava:${Versions.GUAVA}")
   "guiceSupportImplementation"("com.google.inject:guice:4.2.3")
-  api("org.checkerframework:checker-qual:2.4.0")
+  api("org.checkerframework:checker-qual:3.3.0")
 
 }
