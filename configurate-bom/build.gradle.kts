bc8543559bb54638012af259f35d69e4c5a95a11
Zach Levis <zml@aoeu.xyz>
2020-05-10 17:07:40 -0700

    bom: Add ext-kotlin, reorder

--- configurate-bom/build.gradle.kts
+++ configurate-bom/build.gradle.kts
@@ -12,11 +12,12 @@ configurate {
 dependencies {
     constraints {
         api(project(":configurate-core"))
-        api(project(":configurate-xml"))
+        api(project(":configurate-ext-kotlin"))
         api(project(":configurate-gson"))
-        api(project(":configurate-yaml"))
         api(project(":configurate-hocon"))
         api(project(":configurate-json"))
         api(project(":configurate-tool"))
+        api(project(":configurate-xml"))
+        api(project(":configurate-yaml"))
     }
 }
