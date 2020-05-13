c41b6157bd4ae1630796cb2dc80d5dd664100565
Zach Levis <zml@aoeu.xyz>
2020-05-13 00:21:29 -0700

    core: Add main package info file

---
+++ configurate-core/src/main/java/ninja/leaping/configurate/package-info.java
@@ -0,0 +1,23 @@
+/*
+ * Configurate
+ * Copyright (C) zml and Configurate contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * The main package for Configurate.
+ * <p>
+ * This package contains the core elements of Configurate, including nodes, options, and the visitor. Start out with
+ * {@link ninja.leaping.configurate.ConfigurationNode} and {@link ninja.leaping.configurate.loader.ConfigurationLoader}!
+ */
+package ninja.leaping.configurate;
