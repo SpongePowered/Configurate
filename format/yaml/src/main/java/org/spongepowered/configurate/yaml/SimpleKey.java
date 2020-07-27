682507661e0c7448abea51e0ed2ad9926698eab4
zml <zml@aoeu.xyz>
2020-07-27 04:04:42 -0700

    yaml: Import ScannerImpl + SimpleKey from SnakeYAML for modifications

---
+++ format/yaml/src/main/java/org/spongepowered/configurate/yaml/SimpleKey.java
@@ -0,0 +1,97 @@
+/*
+ * Configurate
+ * Copyright (C) zml and Configurate contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.spongepowered.configurate.yaml;
+
+import org.yaml.snakeyaml.error.Mark;
+
+/**
+ * This file is included directly from SnakeYAML so it can be modified
+ * by Configurate. All modifications must be marked by a 'Configurate' comment.
+ * Larger blocks can be marked with 'Configurate start' and
+ * 'Configurate end' comments.
+ *
+ * License for original content:
+ *
+ * Copyright (c) 2008, http://www.snakeyaml.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Simple keys treatment.
+ * <p>
+ * Helper class for {@link ConfigurateScanner}.
+ * </p>
+ *
+ * @see ConfigurateScanner
+ */
+final class SimpleKey {
+    private int tokenNumber;
+    private boolean required;
+    private int index;
+    private int line;
+    private int column;
+    private Mark mark;
+
+    public SimpleKey(int tokenNumber, boolean required, int index, int line, int column, Mark mark) {
+        this.tokenNumber = tokenNumber;
+        this.required = required;
+        this.index = index;
+        this.line = line;
+        this.column = column;
+        this.mark = mark;
+    }
+
+    public int getTokenNumber() {
+        return this.tokenNumber;
+    }
+
+    public int getColumn() {
+        return this.column;
+    }
+
+    public Mark getMark() {
+        return mark;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public boolean isRequired() {
+        return required;
+    }
+
+    @Override
+    public String toString() {
+        return "SimpleKey - tokenNumber=" + tokenNumber + " required=" + required + " index="
+                + index + " line=" + line + " column=" + column;
+    }
+}
