cd1e5b9b68336812cfd16f56e0d6d45fbdf975d6
Zach Levis <zml@aoeu.xyz>
2020-08-06 10:35:44 -0700

    core: Follow symlinks when writing files

--- core/src/main/java/org/spongepowered/configurate/loader/AtomicFiles.java
+++ core/src/main/java/org/spongepowered/configurate/loader/AtomicFiles.java
@@ -65,7 +65,17 @@ public final class AtomicFiles {
      */
     @NonNull
     public static BufferedWriter createAtomicBufferedWriter(@NonNull Path path, final @NonNull Charset charset) throws IOException {
+        // absolute
         path = path.toAbsolutePath();
+
+        // unwrap any symbolic links
+        try {
+            while (Files.isSymbolicLink(path)) {
+                path = Files.readSymbolicLink(path);
+            }
+        } catch (final UnsupportedOperationException | IOException ex) {
+            // ignore
+        }
 
         final Path writePath = getTemporaryPath(path.getParent(), path.getFileName().toString());
         if (Files.exists(path)) {
