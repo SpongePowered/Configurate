a50446420b2a5bc3b988453952fd33a328360512
Zach Levis <zml@aoeu.xyz>
2020-08-09 11:35:46 -0700

    core: Add test for symlink following on write

--- core/src/test/java/org/spongepowered/configurate/loader/AbstractConfigurationLoaderTest.java
+++ core/src/test/java/org/spongepowered/configurate/loader/AbstractConfigurationLoaderTest.java
@@ -16,12 +16,20 @@
  */
 package org.spongepowered.configurate.loader;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 public class AbstractConfigurationLoaderTest {
 
@@ -37,6 +45,30 @@ public class AbstractConfigurationLoaderTest {
         final File tempFile = new File(tempDir.resolve("text5.txt").getRoot().toFile(), "does-not-exist-dont-edit-testdir");
         final TestConfigurationLoader loader = TestConfigurationLoader.builder().setFile(tempFile).build();
         loader.load();
+    }
+
+    @Test
+    public void testSaveFollowsSymbolicLinks(final @TempDir Path tempDir) throws IOException {
+        final Path actualFile = tempDir.resolve(Paths.get("first", "second", "third.json"));
+        Files.createDirectories(actualFile.getParent());
+        final Path layerOne = tempDir.resolve("general.json");
+        final Path layerTwo = tempDir.resolve("general2.json");
+
+        Files.createFile(actualFile);
+        Files.createSymbolicLink(layerOne, actualFile);
+        Files.createSymbolicLink(layerTwo, layerOne);
+
+        try (BufferedWriter writer = AtomicFiles.createAtomicBufferedWriter(layerTwo, StandardCharsets.UTF_8)) {
+            writer.write("I should follow symlinks!\n");
+        }
+
+        // We expect links are preserved, and the underlying file is written to
+        assertTrue(Files.isSymbolicLink(layerTwo));
+        assertTrue(Files.isSymbolicLink(layerOne));
+        assertEquals(layerOne, Files.readSymbolicLink(layerTwo));
+        assertEquals(actualFile, Files.readSymbolicLink(layerOne));
+        assertEquals("I should follow symlinks!", Files.readAllLines(layerTwo, StandardCharsets.UTF_8).stream()
+                .collect(Collectors.joining("\n")));
     }
 
 }
