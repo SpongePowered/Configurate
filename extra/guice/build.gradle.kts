70bac4830a216125f75a6dbd732a7f778a9bf72b
Zach Levis <zml@aoeu.xyz>
2021-03-14 20:23:07 -0700

    build, guice: bump dependencies and Guice version to be able to run tests on Java 16

--- extra/guice/build.gradle.kts
+++ extra/guice/build.gradle.kts
@@ -8,6 +8,8 @@ description = "Guice support for Configurate's object mapper"
 
 dependencies {
     implementation(core())
-    // We are stuck with 4.1.0 because MC has not updated its Guava version
+    // We want to remain compatible with Guice 4 since it's the last upstream version compatible with Minecraft's Guava
     implementation("com.google.inject:guice:4.1.0")
+    // Run tests against Guice 5.0.1 since Sponge uses an adapted fork of it
+    testRuntimeOnly("com.google.inject:guice:5.0.1")
 }
