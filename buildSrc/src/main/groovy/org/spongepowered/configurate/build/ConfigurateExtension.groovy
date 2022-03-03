71c46f73288af619451f370545bc43ac87b0d19c
zml <zml@stellardrift.ca>
2022-03-03 13:57:02 -0800

    build: Tidy up extensions, configure project names for eclipse

--- buildSrc/src/main/groovy/org/spongepowered/configurate/build/ConfigurateExtension.groovy
+++ buildSrc/src/main/groovy/org/spongepowered/configurate/build/ConfigurateExtension.groovy
@@ -4,6 +4,8 @@ import org.gradle.api.model.ObjectFactory
 import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.provider.Property
+import org.gradle.api.tasks.javadoc.Javadoc
+import org.gradle.external.javadoc.StandardJavadocDocletOptions
 
 class ConfigurateExtension {
     final Property<String> autoValueVersion
@@ -17,5 +19,22 @@ class ConfigurateExtension {
     def useAutoValue() {
         this.dh.add(JavaPlugin.COMPILE_ONLY_CONFIGURATION_NAME, "com.google.auto.value:auto-value-annotations:${autoValueVersion.get()}")
         this.dh.add(JavaPlugin.ANNOTATION_PROCESSOR_CONFIGURATION_NAME, "com.google.auto.value:auto-value:${autoValueVersion.get()}")
+    }
+    
+    void applyCommonAttributes(final Javadoc self) {
+        def options = self.options
+        options.encoding = "UTF-8"
+        if (options instanceof StandardJavadocDocletOptions) {
+            options.links(
+                "https://lightbend.github.io/config/latest/api/",
+                "https://fasterxml.github.io/jackson-core/javadoc/2.10/",
+                "https://checkerframework.org/api/",
+                "https://www.javadoc.io/doc/io.leangen.geantyref/geantyref/1.3.11/"
+            )
+
+            options.addBooleanOption("html5", true)
+            options.addStringOption("-release", "8")
+            options.linkSource()
+        }
     }
 }
