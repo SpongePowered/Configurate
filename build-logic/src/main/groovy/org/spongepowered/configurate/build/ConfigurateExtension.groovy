941436c0310c313b1ba547ca7e08d8b1359fd577
zml <zml@stellardrift.ca>
2022-03-03 18:55:13 -0800

    build: Switch to using version catalogs

--- build-logic/src/main/groovy/org/spongepowered/configurate/build/ConfigurateExtension.groovy
+++ build-logic/src/main/groovy/org/spongepowered/configurate/build/ConfigurateExtension.groovy
@@ -1,26 +1,30 @@
 package org.spongepowered.configurate.build
 
-import org.gradle.api.model.ObjectFactory
+import groovy.transform.PackageScope
+import org.gradle.api.GradleException
+import org.gradle.api.artifacts.VersionCatalog
 import org.gradle.api.artifacts.dsl.DependencyHandler
+import org.gradle.api.model.ObjectFactory
 import org.gradle.api.plugins.JavaPlugin
-import org.gradle.api.provider.Property
 import org.gradle.api.tasks.javadoc.Javadoc
 import org.gradle.external.javadoc.StandardJavadocDocletOptions
 
 class ConfigurateExtension {
-    final Property<String> autoValueVersion
+    @PackageScope VersionCatalog libs
     private final DependencyHandler dh
-    
+
     ConfigurateExtension(final ObjectFactory objects, final DependencyHandler dh) {
-        this.autoValueVersion = objects.property(String)
         this.dh = dh
     }
-    
+
     def useAutoValue() {
-        this.dh.add(JavaPlugin.COMPILE_ONLY_CONFIGURATION_NAME, "com.google.auto.value:auto-value-annotations:${autoValueVersion.get()}")
-        this.dh.add(JavaPlugin.ANNOTATION_PROCESSOR_CONFIGURATION_NAME, "com.google.auto.value:auto-value:${autoValueVersion.get()}")
+        if (!libs) {
+            throw new GradleException("Tried to useAutoValue() without the Java plugin present! libs is not set.")
+        }
+        this.dh.add(JavaPlugin.COMPILE_ONLY_CONFIGURATION_NAME, libs.findLibrary("autoValue-annotations").get())
+        this.dh.add(JavaPlugin.ANNOTATION_PROCESSOR_CONFIGURATION_NAME, libs.findLibrary("autoValue").get())
     }
-    
+
     void applyCommonAttributes(final Javadoc self) {
         def options = self.options
         options.encoding = "UTF-8"
