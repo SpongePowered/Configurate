3ad6a692a9357cd9d0e9718991a01c9ef11e2949
Kyle Wood <kyle@denwav.dev>
2023-07-05 00:01:38 -0500

    Add Kotlin extensions and helpers around ObjectMapper and Processors (#409)

--- extra/kotlin/src/main/kotlin/org/spongepowered/configurate/kotlin/extensions/ObjectMapperExtensions.kt
+++ extra/kotlin/src/main/kotlin/org/spongepowered/configurate/kotlin/extensions/ObjectMapperExtensions.kt
@@ -19,6 +19,9 @@ package org.spongepowered.configurate.kotlin.extensions
 import org.spongepowered.configurate.kotlin.typeTokenOf
 import org.spongepowered.configurate.objectmapping.ObjectMapper
 import org.spongepowered.configurate.objectmapping.ObjectMapper.Factory
+import org.spongepowered.configurate.objectmapping.ObjectMapper.Factory.Builder
+import org.spongepowered.configurate.objectmapping.meta.Constraint
+import org.spongepowered.configurate.objectmapping.meta.Processor
 import org.spongepowered.configurate.serialize.TypeSerializer
 import org.spongepowered.configurate.serialize.TypeSerializerCollection
 import kotlin.reflect.KClass
@@ -55,3 +58,69 @@ inline fun <reified T> TypeSerializerCollection.get(): TypeSerializer<T>? {
 fun <T : Any> TypeSerializerCollection.get(type: KClass<T>): TypeSerializer<T>? {
     return get(type.java)
 }
+
+// Processors
+/**
+ * Register a [Processor] that will process fields after write, accepting Kotlin types.
+ * @see Builder.addProcessor
+ */
+fun <A : Annotation> Builder.addProcessor(definition: KClass<A>, factory: Processor.Factory<A, Any?>): Builder =
+    addProcessor(definition.java, factory)
+
+/**
+ * Register a [Processor] that will process fields after write, accepting Kotlin types.
+ * @see Builder.addProcessor
+ */
+fun <A : Annotation, T : Any> Builder.addProcessor(
+    definition: KClass<A>,
+    valueType: KClass<T>,
+    factory: Processor.Factory<A, T?>,
+): Builder = addProcessor(definition.java, valueType.java, factory)
+
+/**
+ * Register a [Processor] that will process fields after write, accepting parameterized types.
+ * @see Builder.addProcessor
+ */
+inline fun <reified A : Annotation> Builder.addProcessor(factory: Processor.Factory<A, Any?>) =
+    addProcessor(A::class, factory)
+
+/**
+ * Register a [Processor] that will process fields after write, accepting parameterized types.
+ * @see Builder.addProcessor
+ */
+@JvmName("addProcessorFull")
+inline fun <reified A : Annotation, reified T : Any> Builder.addProcessor(factory: Processor.Factory<A, T?>) =
+    addProcessor(A::class, T::class, factory)
+
+// Constraints
+/**
+ * Register a [Constraint] that will be used to validate fields, accepting Kotlin types.
+ * @see Builder.addConstraint
+ */
+fun <A : Annotation> Builder.addConstraint(definition: KClass<A>, factory: Constraint.Factory<A, Any?>): Builder =
+    addConstraint(definition.java, factory)
+
+/**
+ * Register a [Constraint] that will be used to validate fields, accepting Kotlin types.
+ * @see Builder.addConstraint
+ */
+fun <A : Annotation, T : Any> Builder.addConstraint(
+    definition: KClass<A>,
+    valueType: KClass<T>,
+    factory: Constraint.Factory<A, T?>,
+): Builder = addConstraint(definition.java, valueType.java, factory)
+
+/**
+ * Register a [Constraint] that will be used to validate fields, accepting parameterized types.
+ * @see Builder.addConstraint
+ */
+inline fun <reified A : Annotation> Builder.addConstraint(factory: Constraint.Factory<A, Any?>) =
+    addConstraint(A::class, factory)
+
+/**
+ * Register a [Constraint] that will be used to validate fields, accepting parameterized types.
+ * @see Builder.addConstraint
+ */
+@JvmName("addConstraintFull")
+inline fun <reified A : Annotation, reified T : Any> Builder.addConstraint(factory: Constraint.Factory<A, T?>) =
+    addConstraint(A::class, T::class, factory)
