ebf54d77dc0aad4c053c8ede6135b63322f31012
zml <zml@stellardrift.ca>
2023-10-14 15:13:44 -0700

    chore(build-logic): Apply CodeNarc for groovy source files

--- build-logic/src/main/groovy/org/spongepowered/configurate/build/ConfigurateExtension.groovy
+++ build-logic/src/main/groovy/org/spongepowered/configurate/build/ConfigurateExtension.groovy
@@ -4,41 +4,42 @@ import groovy.transform.PackageScope
 import org.gradle.api.GradleException
 import org.gradle.api.artifacts.VersionCatalog
 import org.gradle.api.artifacts.dsl.DependencyHandler
-import org.gradle.api.model.ObjectFactory
 import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.tasks.javadoc.Javadoc
 import org.gradle.external.javadoc.StandardJavadocDocletOptions
 
 class ConfigurateExtension {
+
     @PackageScope VersionCatalog libs
     private final DependencyHandler dh
 
-    ConfigurateExtension(final ObjectFactory objects, final DependencyHandler dh) {
+    ConfigurateExtension(final DependencyHandler dh) {
         this.dh = dh
     }
 
-    def useAutoValue() {
+    void useAutoValue() {
         if (!libs) {
-            throw new GradleException("Tried to useAutoValue() without the Java plugin present! libs is not set.")
+            throw new GradleException('Tried to useAutoValue() without the Java plugin present! libs is not set.')
         }
-        this.dh.add(JavaPlugin.COMPILE_ONLY_CONFIGURATION_NAME, libs.findLibrary("autoValue-annotations").get())
-        this.dh.add(JavaPlugin.ANNOTATION_PROCESSOR_CONFIGURATION_NAME, libs.findLibrary("autoValue").get())
+        this.dh.add(JavaPlugin.COMPILE_ONLY_CONFIGURATION_NAME, libs.findLibrary('autoValue-annotations').get())
+        this.dh.add(JavaPlugin.ANNOTATION_PROCESSOR_CONFIGURATION_NAME, libs.findLibrary('autoValue').get())
     }
 
     void applyCommonAttributes(final Javadoc self) {
         def options = self.options
-        options.encoding = "UTF-8"
+        options.encoding = 'UTF-8'
         if (options instanceof StandardJavadocDocletOptions) {
             options.links(
-                "https://lightbend.github.io/config/latest/api/",
-                "https://fasterxml.github.io/jackson-core/javadoc/2.10/",
-                "https://www.javadoc.io/doc/org.checkerframework/checker-qual/${libs.findVersion("checkerQual").get().requiredVersion}/",
-                "https://www.javadoc.io/doc/io.leangen.geantyref/geantyref/${libs.findVersion("geantyref").get().requiredVersion}/"
+                'https://lightbend.github.io/config/latest/api/',
+                'https://fasterxml.github.io/jackson-core/javadoc/2.10/',
+                "https://www.javadoc.io/doc/org.checkerframework/checker-qual/${libs.findVersion('checkerQual').get().requiredVersion}/",
+                "https://www.javadoc.io/doc/io.leangen.geantyref/geantyref/${libs.findVersion('geantyref').get().requiredVersion}/"
             )
 
-            options.addBooleanOption("html5", true)
-            options.addStringOption("-release", "8")
+            options.addBooleanOption('html5', true)
+            options.addStringOption('-release', '8')
             options.linkSource()
         }
     }
+
 }
--- build-logic/src/main/groovy/org/spongepowered/configurate/build/SignJarTask.groovy
+++ build-logic/src/main/groovy/org/spongepowered/configurate/build/SignJarTask.groovy
@@ -46,4 +46,5 @@ abstract class SignJarTask extends Jar {
             )
         }
     }
+
 }
